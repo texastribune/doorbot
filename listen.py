f32a83dd335fed77eb84ab9db5476e4231c5a102
tbrockhoeft <tbrockhoeft@texastribune.org>
2018-03-30 18:14:35 -0500

    Fix parens.

--- listen.py
+++ listen.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 import subprocess
+import knockknock
 
 if __name__ == '__main__':
     while True:
         time.sleep(20)
-        subprocess.call(['python knockknock.py'])
+        knockknock.post_to_slack()
